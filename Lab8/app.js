f174f22306f0a53ddee23f4b5c9c8f381438cd9e
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-02-22 10:07:22 -0600

    app imprimir elementos por tiempo

--- Lab8/app.js
+++ Lab8/app.js
@@ -3,3 +3,11 @@ console.log("Hello World");
 
 const filesystem = require('fs');
 filesystem.writeFileSync('hola.txt', 'ooohhh mai gawwd');
+
+const arreglo = [2000, 400, 60, 800, 9, 90, 50];
+
+for (let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
