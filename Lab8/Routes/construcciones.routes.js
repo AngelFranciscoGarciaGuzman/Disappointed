d6b005162006b73169ca1d2787b75d87a9b0199f
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-03-04 10:12:57 -0600

    Lab 13

--- Lab8/Routes/construcciones.routes.js
+++ Lab8/Routes/construcciones.routes.js
@@ -1,32 +1,9 @@
 const express = require('express');
-
 const router = express.Router();
+const construccionesController = require('../controllers/construcciones.controller');
 
-const construcciones = [
-    {
-        nombre: "casa", 
-        imagen: "https://media1.tenor.com/m/jJtL4af7Lx8AAAAC/cat-cats.gif",
-    }
-];
-router.get('/construir', (request, response, next) => {
-    response.render('construir'); 
-});
-
-
-router.post('/construir', (request, response, next) => {
-    console.log(request.body);
-    construcciones.push(request.body);
-    response.redirect('/');
-});
-
-
-router.get('/', (request, response, next) => {
-    console.log('Ruta /');
-
-    response.render('construcciones', {
-        construcciones: construcciones,
-    })
-});
-
+router.get('/construir', construccionesController.get_construir);
+router.post('/construir', construccionesController.post_construir);
+router.get('/', construccionesController.get_root);
 
 module.exports = router;
