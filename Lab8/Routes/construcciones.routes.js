a3f6b9bfa290a8da2c68e45cd7efc0a513e3c718
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-03-05 11:18:18 -0600

    lab 14

--- Lab8/Routes/construcciones.routes.js
+++ Lab8/Routes/construcciones.routes.js
@@ -6,6 +6,4 @@ router.get('/construir', construccionesController.get_construir);
 router.post('/construir', construccionesController.post_construir);
 router.get('/', construccionesController.get_root);
 
-router.get('/emuladores', construccionesController.get_emuladores);
-
 module.exports = router;
--- Lab8/controllers/construcciones.controller.js
+++ Lab8/controllers/construcciones.controller.js
@@ -1,19 +1,22 @@
 const Construccion = require('../models/construccion.model');
 
-const Emulador = require('../models/emulador.model');
-
 exports.get_construir = (request, response, next) => {
-    response.render('construir'); 
+    response.render('construir', {
+        username: request.session.username || '',
+    }); 
 };
+
 exports.post_construir = (request, response, next) => {
     console.log(request.body);
     const construccion = 
     new Construccion(request.body.nombre, request.body.imagen);
     construccion.save();
-    
-    response.setHeader('Set-Cookie', 'ultima_construccion=' + request.body.nombre + "; HttpOnly");
+
+    response.setHeader('Set-Cookie', 
+        'ultima_construccion=' + request.body.nombre + '; HttpOnly');
     response.redirect('/');
 };
+
 exports.get_root = (request, response, next) => {
     console.log('Ruta /');
     let ultima_construccion = request.get('Cookie');
@@ -26,11 +29,6 @@ exports.get_root = (request, response, next) => {
     response.render('construcciones', {
         construcciones: Construccion.fetchAll(),
         ultima_construccion: ultima_construccion,
+        username: request.session.username || '',
     });
 }
-
-exports.get_emuladores = (request, response, next) => {
-    response.render('emuladores', {
-        emuladores: Emulador.fetchAll(),
-    }); 
-};
