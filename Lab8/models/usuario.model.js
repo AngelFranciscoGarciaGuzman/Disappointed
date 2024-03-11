27bb803ed90996f8657cc90adb912c6ddb89cbff
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-03-11 10:53:42 -0600

    metodo para autenticar

--- Lab8/models/usuario.model.js
+++ Lab8/models/usuario.model.js
@@ -1,4 +1,6 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
+
 module.exports = class Usuario {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(mi_username, mi_password) {
@@ -7,14 +9,17 @@ module.exports = class Usuario {
     }
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute(
-            'INSERT INTO usuario (username, password) VALUES (?, ?)',
-            [this.username, this.password]
-        );
+        return bcrypt.hash(this.password, 12).then((password_cifrado) => {
+            return db.execute(
+                'INSERT INTO usuario (username, password) VALUES (?, ?)',
+                [this.username, password_cifrado]
+                );
+            })
+            .catch((error) => console.log(error)); 
     }
     static fetchOne(username, password) {
         return db.execute(
-            'SELECT * FROM usuario WHERE username=? AND password = ?', 
-            [username, password]);
+            'SELECT * FROM usuario WHERE username=?', 
+            [username]);
     }
 }
